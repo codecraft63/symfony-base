ed58af281ff9821a52b67527987f699eab1dc3cd
Ramon Soares <ramon@codecraft63.com>
2017-08-23 15:34:56 -0300

    add web profiler bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
     'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
 ];
