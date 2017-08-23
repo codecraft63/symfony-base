8e1d1e92f1ec5b96847b45b9d01039b494c3cee4
Ramon Soares <ramon@codecraft63.com>
2017-08-23 14:25:59 -0300

    Initial commit

---
+++ config/bundles.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
+    'Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle' => ['all' => true],
+    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
+    'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+];
