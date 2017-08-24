868d1516fb6ab606905ab5f151f03e59ba2f2d33
Ramon Soares <ramon@codecraft63.com>
2017-08-24 09:38:55 -0300

    add postcss webpack support

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ Encore
     // allow sass/scss files to be processed
     .enableSassLoader()
     .enableVueLoader()
+    .enablePostCssLoader()
 
     // allow legacy applications to use $/jQuery as a global variable
     .autoProvidejQuery()
