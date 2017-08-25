f56fd385cf144e17a79a718383783722c52df100
Ramon Soares <ramon@codecraft63.com>
2017-08-25 16:21:29 -0300

    Add doctrine migrations support

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
     'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    'Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle' => ['all' => true],
 ];
