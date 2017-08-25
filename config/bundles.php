9c906b3cdd4238c933ea796b65d5ac864f4f9cdb
Ramon Soares <ramon@codecraft63.com>
2017-08-25 15:00:30 -0300

    remove some packages and initial DDD structure

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,6 @@ return [
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
     'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
-    'Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle' => ['all' => true],
     'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
     'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
