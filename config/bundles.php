fae1760b1d8f95bf486e0272e5993e76008710db
Ramon Soares <ramon@codecraft63.com>
2017-08-25 19:37:53 -0300

    remove doctrine migration package

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
     'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
-    'Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle' => ['all' => true],
 ];
