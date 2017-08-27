a5a4f587fedc70a0ce6e4862110cf3a75b3fc6b7
Ramon Soares <ramon@codecraft63.com>
2017-08-27 12:22:10 -0300

    add Tactician CommandBus package

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
     'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    'League\Tactician\Bundle\TacticianBundle' => ['all' => true],
 ];
